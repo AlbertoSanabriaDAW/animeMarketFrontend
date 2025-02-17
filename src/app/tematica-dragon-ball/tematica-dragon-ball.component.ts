9bb32692935c93c7a01662a595bea8e767e756f8
Alberto <sanabriaalbertog@gmail.com>
2025-02-17 19:52:22 +0100

    Parece que funciona el filtro de dragon ball

--- src/app/tematica-dragon-ball/tematica-dragon-ball.component.ts
+++ src/app/tematica-dragon-ball/tematica-dragon-ball.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {ProductoModelo} from '../modelos/producto.modelo';
+import {ProductoService} from '../services/producto.service';
 
 @Component({
   selector: 'app-tematica-dragon-ball',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './tematica-dragon-ball.component.html',
   styleUrl: './tematica-dragon-ball.component.css'
 })
-export class TematicaDragonBallComponent {
+export class TematicaDragonBallComponent implements OnInit {
+
+  protected productos: ProductoModelo[] = [
+
+  ];
+  constructor(private productosService: ProductoService) {
+  }
+
+  ngOnInit() {
+    this.productosService.getProductosDragonball().subscribe(data => {
+      this.productos = data;
+      console.log(data);
+    });
+  }
+
+  anyadirAlCarrito(producto: ProductoModelo): void {
+    console.log('Añadiendo al carrito:', producto);
+  }
 
 }
