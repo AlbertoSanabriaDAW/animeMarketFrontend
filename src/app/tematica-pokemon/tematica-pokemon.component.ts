c5dc456e5d9416ac682ee7ffcc65cd7f0869a0f0
Alberto <sanabriaalbertog@gmail.com>
2025-02-17 20:20:00 +0100

    Parece que funciona el filtro de pokemon funciona y se cambia el fondo de goku

--- src/app/tematica-pokemon/tematica-pokemon.component.ts
+++ src/app/tematica-pokemon/tematica-pokemon.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {ProductoModelo} from '../modelos/producto.modelo';
+import {ProductoService} from '../services/producto.service';
 
 @Component({
   selector: 'app-tematica-pokemon',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './tematica-pokemon.component.html',
   styleUrl: './tematica-pokemon.component.css'
 })
-export class TematicaPokemonComponent {
+export class TematicaPokemonComponent implements OnInit {
+
+  protected productos: ProductoModelo[] = [
+
+  ];
+  constructor(private productosService: ProductoService) {
+  }
+
+  ngOnInit() {
+    this.productosService.getProductosPokemon().subscribe(data => {
+      this.productos = data;
+      console.log(data);
+    });
+  }
+
+  anyadirAlCarrito(producto: ProductoModelo): void {
+    console.log('Añadiendo al carrito:', producto);
+  }
 
 }
