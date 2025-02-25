38e5496b15043ebc7fd12f73f005b2660bf8e8ed
Alberto <sanabriaalbertog@gmail.com>
2025-02-25 17:51:28 +0100

    Configuracion para que boton de agregar funcione (los primeros no van por que hay ids dublicados, en la bdd hasta el 6 y luego se han creado por frontend dos

--- src/app/services/usuariosService.ts
+++ src/app/services/usuariosService.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {RegistroUsuario} from '../modelos/usuario.modelo';
-import {ProductoModelo} from '../modelos/producto.modelo';
+// import {RegistroUsuario} from '../modelos/usuario.modelo';
+// import {ProductoModelo} from '../modelos/producto.modelo';
 
 @Injectable({
   providedIn: 'root'
