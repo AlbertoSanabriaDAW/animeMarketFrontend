64306dc6e3e3801618f1deda2bdc6d17155b593b
Alberto <sanabriaalbertog@gmail.com>
2025-02-25 20:54:13 +0100

    primer modelo de carrito (falta pulor mucho)

--- src/app/carrito/carrito.component.ts
+++ src/app/carrito/carrito.component.ts
@@ -1,22 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {CarritoService} from '../services/carrito.service';
 import {AuthService} from '../services/auth.service';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-carrito',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
 export class CarritoComponent implements OnInit {
+  carritos: any;
 
   constructor(private carritoService: CarritoService, private authService: AuthService) {}
 
   ngOnInit() {
-    this.carritoService.obtenerCarrito().subscribe({
-      next: (response) => console.log(response),
-      error: (error) => console.error(error)
+    this.carritoService.obtenerCarrito().subscribe(data => {
+      this.carritos = data;
+      console.log(data);
     });
   }
+
+  // ngOnInit() {
+  //   this.carritoService.obtenerCarrito().subscribe({
+  //     next: (response) => console.log(response),
+  //     error: (error) => console.error(error)
+  //   });
+  // }
 
 }
