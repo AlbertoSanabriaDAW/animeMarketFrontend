7976bc980f76c00a6d6f181ef5fdc1fee5ae3f58
Alberto <sanabriaalbertog@gmail.com>
2025-05-13 19:30:08 +0200

    mediados 13.05

--- src/app/componentes/crear-producto/crear-producto.component.ts
+++ src/app/componentes/crear-producto/crear-producto.component.ts
@@ -1,87 +1,41 @@
-// import { Component, EventEmitter, Input, Output } from '@angular/core';
-// import {ProductoModelo} from '../../modelos/producto.modelo';
-// import { HttpClient } from '@angular/common/http';
-// import {FormsModule} from '@angular/forms';
-// import {NgIf} from '@angular/common';
-//
-// @Component({
-//   selector: 'app-crear-producto',
-//   templateUrl: './crear-producto.component.html',
-//   imports: [
-//     FormsModule,
-//     NgIf
-//   ],
-//   styleUrls: ['./crear-producto.component.css']
-// })
-// export class CrearProductoComponent {
-//   @Input() isOpen: boolean = false;
-//   @Output() closeEvent = new EventEmitter<void>();
-//   @Output() productoCreado = new EventEmitter<ProductoModelo>(); // Esto es necesario para que Angular detecte el evento
-//
-//   producto: ProductoModelo = {
-//     id: 1500,
-//     nombre: 'NuevoProducto_1',
-//     descripcion: 'Este es un producto nuevo',
-//     precio: 1,
-//     imagen: 'IproductoNuevo.png',
-//     id_tematica: 1
-//   };
-//
-//   constructor(private http: HttpClient) {}
-//
-//   close() {
-//     this.isOpen = false;
-//     this.closeEvent.emit();
-//   }
-//
-//   crearProducto() {
-//     this.http.post('http://localhost:8000/api/productos/create', this.producto)
-//       .subscribe(response => {
-//         console.log('Producto creado exitosamente:', response);
-//         this.productoCreado.emit(this.producto); // Emitimos el evento al crearse un producto exitosamente
-//         this.close();
-//       }, error => {
-//         console.error('Error al crear el producto:', error);
-//       });
-//   }
-// }
-
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
-import {FormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-crear-producto',
   templateUrl: './crear-producto.component.html',
+  standalone: true,
   imports: [
     FormsModule,
     NgIf
   ],
-  styleUrls: ['./crear-producto.component.css']
+  //styleUrls: ['./crear-producto.component.css']
 })
 export class CrearProductoComponent {
   @Input() isOpen: boolean = false;
   @Output() closeEvent = new EventEmitter<void>();
   @Output() productoCreado = new EventEmitter<void>();
-  producto = {
+
+    producto = {
     nombre: 'NuevoProducto_1',
     descripcion: 'Este es un producto nuevo',
     precio: 1,
     imagen: 'IproductoNuevo.png',
-    id_tematica: 1
-  };
-
+    id_tematicas: 1,
+    valoraciones: 0,
+    numeroValoraciones: 0,
+    };
 
   constructor(private http: HttpClient) {}
 
   crearProducto() {
-    const url = '/api/productos/create';
-    const headers = { 'Content-Type': 'application/json' };
+    const url = '/api/productos/create?XDEBUG_SESSION_START=19561';
 
-    this.http.post(url, JSON.stringify(this.producto), { headers })
+    this.http.post(url, this.producto)
       .pipe(
         catchError(err => {
           console.error('Error al crear producto:', err);
@@ -93,20 +47,18 @@ export class CrearProductoComponent {
         if (response) {
           console.log('Producto creado exitosamente:', response);
           alert('Producto creado exitosamente.');
+          this.productoCreado.emit();
           this.close();
         }
       });
   }
 
-  abrirFormulario() {
-    this.isOpen = true;
-  }
-
-
   close() {
     this.isOpen = false;
+    this.closeEvent.emit();
   }
 }
+
 
 // ----------------------------------------------------------
 
